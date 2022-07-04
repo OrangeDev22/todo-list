86ddf1cf9bcc4b3715fe838aeeaaa8a532763143
OrangeDev22 <OrangeDev8@gmail.com>
2022-07-04 15:58:01 -0600

    feat: implementing signin and signup logic

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,11 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
-import { AuthDto } from './dto';
+import { SigninDto, SignupDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup(dto: AuthDto) {}
-  signin(dto: AuthDto) {}
+  async signup(dto: SignupDto) {
+    try {
+      const hash = await argon.hash(dto.password);
+      const user = await this.prisma.user.create({
+        data: { email: dto.email, hash, username: dto.username },
+      });
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
+  }
+  async signin(dto: SigninDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+    console.log('--user', user);
+    if (!user) throw new ForbiddenException('Crendentials Incorrect');
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    if (!pwMatches) {
+      throw new ForbiddenException('Crendentials Incorrect');
+    }
+    delete user.hash;
+    return user;
+  }
 }
