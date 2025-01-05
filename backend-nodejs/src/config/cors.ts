cff11ee15908ea169a77ffa444bf619adc1c3ff5
Gabriel Matute <orangedev8@gmail.com>
2025-01-05 10:03:16 -0600

    adding origin option for cors

--- backend-nodejs/src/config/cors.ts
+++ backend-nodejs/src/config/cors.ts
@@ -1,5 +1,5 @@
 export const corsOptions = {
-  origin: process.env.FRONT_END_ORIGIN,
+  origin: [process.env.FRONT_END_ORIGIN, "http://localhost:3000"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
